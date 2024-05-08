068f142cd62ee10896d0ac8a3f9b805b2a3da2ea
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-08 00:13:31 -0300

    Se reemplaza logo en el Header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -11,6 +11,7 @@ import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/images/SoundRentals-logo.webp";
 
 const Header = () => {
   const [anchorElNav, setAnchorElNav] = useState(null);
@@ -27,26 +28,7 @@ const Header = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            SoundRentals
-          </Typography>
-
-          {/* Menu */}
+          {/* Mobile menu */}
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -90,25 +72,15 @@ const Header = () => {
               </MenuItem>
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            SoundRentals
-          </Typography>
+
+          <Box flexGrow={1} component={Link} to="/">
+            <Box
+              component="img"
+              src={Logo}
+              alt="SountRentals"
+              sx={{ maxWidth: { xs: "160px", md: "200px" } }}
+            />
+          </Box>
 
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             <Button
