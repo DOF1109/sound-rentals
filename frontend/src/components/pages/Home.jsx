188b03ba67b9ebdcc57e0b69190f358a6f8742b1
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-09 19:30:09 -0300

    Se modifica carousel

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,10 +1,17 @@
 import { Box } from "@mui/material";
 import SearchInput from "../common/SearchInput";
+import EmblaCarousel from "../common/EmblaCarousel/EmblaCarousel";
+import "../../styles/embla.css";
+
+const OPTIONS = { loop: true };
+const SLIDE_COUNT = 5;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const Home = () => {
   return (
     <Box>
       <SearchInput />
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
       Home
     </Box>
   );
