68f8b9cbab5ada71563aaf7885bd139dee0478dc
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-14 19:50:48 -0300

    Se modifica estilos en Recomendados

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { Box, Container, Typography } from "@mui/material";
 import SearchInput from "../common/SearchInput";
-import EmblaCarousel from "../common/EmblaCarousel/EmblaCarousel";
-import "../../styles/embla.css";
+import EmblaCategoryCarousel from "../common/EmblaCarousel/EmblaCategoryCarousel";
+import EmblaRecommendedCarousel from "../common/EmblaCarousel/EmblaRecommendedCarousel";
 import Img1 from "../../assets/images/image-1-sr.webp";
 import Img2 from "../../assets/images/image-2.webp";
 import Img3 from "../../assets/images/image-3.webp";
 import theme from "../../styles/themeConfig";
 
 const OPTIONS = { loop: true };
+const SLIDE_COUNT = 6;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const images = [Img1, Img2, Img3];
 
@@ -17,13 +19,13 @@ const Home = () => {
       <Box component="section" className="gradientPrimaryBox" pt={1} pb={4}>
         <Container>
           <SearchInput />
-          <EmblaCarousel slides={images} options={OPTIONS} />
+          <EmblaCategoryCarousel slides={images} options={OPTIONS} />
         </Container>
       </Box>
       <Box
         component="section"
         py={4}
-        sx={{ background: theme.palette.light.main, py: 4 }}
+        sx={{ background: theme.palette.light.main, py: 6 }}
       >
         <Container>
           <Typography
@@ -31,10 +33,12 @@ const Home = () => {
             variant="h5"
             fontWeight={600}
             color={theme.palette.background.default}
+            mb={6}
             className="underline-text"
           >
             RECOMENDADOS
           </Typography>
+          <EmblaRecommendedCarousel slides={SLIDES} options={OPTIONS} />
         </Container>
       </Box>
     </>
