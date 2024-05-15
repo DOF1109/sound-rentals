8db42d54e05c56c6023915292036092529903f83
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-15 16:56:03 -0300

    Se agrega politica de CORS en backend

--- backend/src/main/java/com/backend/soundrentals/WebConfig.java
+++ backend/src/main/java/com/backend/soundrentals/WebConfig.java
@@ -1,2 +1,17 @@
-package com.backend.soundrentals;public class WebConfig {
+package com.backend.soundrentals;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true);
+    }
 }
