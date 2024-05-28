e0be0984886cf5c2c7c935bb0670253ff361bacd
Daniel Omar Frias <friasdaniel.1109@gmail.com>
2024-05-28 00:15:43 -0300

    Se limpia Masonry de imagenes

--- frontend/src/components/common/ImageMasonry.jsx
+++ frontend/src/components/common/ImageMasonry.jsx
@@ -1,34 +1,18 @@
-// import * as React from "react";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Masonry from "@mui/lab/Masonry";
-import { styled } from "@mui/material/styles";
 
-const Label = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(0.5),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  borderBottomLeftRadius: 0,
-  borderBottomRightRadius: 0,
-}));
-
-export default function ImageMasonry() {
+export default function ImageMasonry(images) {
   return (
-    // <Box sx={{ width: 500, minHeight: 829 }}>
     <Masonry columns={3} spacing={2} sx={{ m: "auto" }}>
       {itemData.map((item, index) => (
         <div key={index}>
-          <Label>{index + 1}</Label>
           <img
             srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
             src={`${item.img}?w=162&auto=format`}
-            alt={item.title}
+            alt="Imágen del Dj"
             loading="lazy"
             style={{
-              borderBottomLeftRadius: 4,
-              borderBottomRightRadius: 4,
+              borderRadius: 4,
+              borderRadius: 4,
               display: "block",
               width: "100%",
             }}
@@ -36,7 +20,6 @@ export default function ImageMasonry() {
         </div>
       ))}
     </Masonry>
-    // </Box>
   );
 }
 
