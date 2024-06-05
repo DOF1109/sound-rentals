b12aad3081c5c40b6b4119499b4f95a7aae52c60
Daniel Omar Frias <friasdaniel.1109@gmail.com>
2024-06-04 21:48:15 -0300

    Se modifica dashboard de usuarios

--- frontend/src/components/pages/ManageUsers.jsx
+++ frontend/src/components/pages/ManageUsers.jsx
@@ -8,54 +8,32 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import { useState } from "react";
+import theme from "../../styles/themeConfig";
+import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
 
 const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toFixed(2),
-  },
+  { id: "name", label: "Nombre", minWidth: 170 },
+  { id: "email", label: "Email", minWidth: 170 },
+  { id: "isAdmin", label: "Es administrador?", minWidth: 170 },
+  //   { id: "changeRol", label: "Cambiar rol", minWidth: 170 },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
 const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
+  { name: "Usuario 1", email: "usuario1@example.com", isAdmin: true },
+  { name: "Usuario 2", email: "usuario2@example.com", isAdmin: false },
+  { name: "Usuario 3", email: "usuario3@example.com", isAdmin: true },
+  { name: "Usuario 4", email: "usuario4@example.com", isAdmin: false },
+  { name: "Usuario 5", email: "usuario5@example.com", isAdmin: true },
+  { name: "Usuario 6", email: "usuario6@example.com", isAdmin: false },
+  { name: "Usuario 7", email: "usuario7@example.com", isAdmin: true },
+  { name: "Usuario 8", email: "usuario8@example.com", isAdmin: false },
+  { name: "Usuario 9", email: "usuario9@example.com", isAdmin: true },
+  { name: "Usuario 10", email: "usuario10@example.com", isAdmin: false },
+  { name: "Usuario 11", email: "usuario11@example.com", isAdmin: true },
+  { name: "Usuario 12", email: "usuario12@example.com", isAdmin: false },
+  { name: "Usuario 13", email: "usuario13@example.com", isAdmin: true },
+  { name: "Usuario 14", email: "usuario14@example.com", isAdmin: false },
+  { name: "Usuario 15", email: "usuario15@example.com", isAdmin: true },
 ];
 
 const ManageUsers = () => {
@@ -72,7 +50,7 @@ const ManageUsers = () => {
   };
 
   return (
-    <Container sx={{py:5}}>
+    <Container sx={{ py: 5 }}>
       <Paper sx={{ width: "100%", overflow: "hidden" }}>
         <TableContainer sx={{ maxHeight: 440 }}>
           <Table stickyHeader aria-label="sticky table">
@@ -81,12 +59,15 @@ const ManageUsers = () => {
                 {columns.map((column) => (
                   <TableCell
                     key={column.id}
-                    align={column.align}
+                    align="center"
                     style={{ minWidth: column.minWidth }}
                   >
                     {column.label}
                   </TableCell>
                 ))}
+                <TableCell align="center" style={{ minWidth: 170 }}>
+                  Cambiar rol
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -98,18 +79,23 @@ const ManageUsers = () => {
                       hover
                       role="checkbox"
                       tabIndex={-1}
-                      key={row.code}
+                      key={row.email}
                     >
                       {columns.map((column) => {
                         const value = row[column.id];
                         return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
+                          <TableCell key={column.id} align="center">
+                            {column.id !== "isAdmin"
+                              ? value
+                              : row.isAdmin
+                              ? "Si"
+                              : "No"}
                           </TableCell>
                         );
                       })}
+                      <TableCell align="center">
+                        <ChangeCircleIcon />
+                      </TableCell>
                     </TableRow>
                   );
                 })}
