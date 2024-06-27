9390b5ea058431b5fc8f02dc29cefa676c5078a6
leonardoturriagovargas <leonardoturriagovargas@gmail.com>
2024-06-27 20:56:31 +0000

    Mejora query busqueda Dj por Ciudad,Estilo y Fechas

--- backend/src/main/java/com/backend/soundrentals/repository/ReservaRepository.java
+++ backend/src/main/java/com/backend/soundrentals/repository/ReservaRepository.java
@@ -21,7 +21,7 @@ public interface ReservaRepository extends JpaRepository<Reserva, Long> {
     @Query("SELECT DISTINCT dj " +
             "FROM Dj dj " +
             "WHERE (:ciudad IS NULL OR dj.ciudad.id = :ciudad) " +
-            "WHERE (:estilo IS NULL OR dj.estilo.id = :estilo) " +
+            "AND (:estilo IS NULL OR dj.estilo.id = :estilo) " +
             "AND NOT EXISTS (" +
             "    SELECT r " +
             "    FROM Reserva r " +
